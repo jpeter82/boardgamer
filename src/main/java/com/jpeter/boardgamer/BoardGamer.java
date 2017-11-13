84986a29e438fa6dc3bf4b42d5555f41583eaa26
Peter Juhasz <jpeter82@gmail.com>
2017-11-13 21:39:00 +0100

    Initialize project

---
+++ src/main/java/com/jpeter/boardgamer/BoardGamer.java
@@ -0,0 +1,9 @@
+package com.jpeter.boardgamer;
+
+public class BoardGamer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+}
---
+++ src/main/java/com/jpeter/boardgamer/Config.java
@@ -0,0 +1,4 @@
+package com.jpeter.boardgamer;
+
+public class Config {
+}
